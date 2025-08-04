dee6d6d1472d9e3e89254c6961a0f957f6d8f3eb
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2025-08-04 21:43:59 +0800

    MLCC-DAYSHADE-PROJECT-PAGE

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+
 export default function Page() {
   return (
     <div className="min-h-screen bg-black text-white container mx-auto px-4 py-4">
       <h1>test 123</h1>
+      <a href="/projects" className="inline-block mt-4 px-4 py-2 bg-white text-black rounded hover:bg-gray-200 transition">Go to Projects Page</a>
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    formats: ['image/webp', 'image/avif'],
+    remotePatterns: [],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
