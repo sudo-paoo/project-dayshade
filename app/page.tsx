39483fe50107bc2cc464b6878641a706ea83a9bc
minty <eithanmathewmalonzo@gmail.com>
2025-08-05 15:45:27 +0800

    First commit - made first div ad added logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import { Separator } from "@/components/ui/separator";
+import { GlassContainer } from "@/components/shared/glass-container";
+import Image from "next/image";
+import Button from "@/components/button";
+import { RippleButton } from "@/components/magicui/ripple-button";
+
 export default function Page() {
   return (
-    <div className="min-h-screen bg-black text-white container mx-auto px-4 py-4">
-      <h1>test 123</h1>
-    </div>
-  )
+    <>
+      <div className="min-h-screen w-full bg-black text-white container mx-auto px-4 py-4 bg-[url('/assets/circles.png')] bg-cover">
+        {/* Mobile View  of Home*/}
+        {/* Opening Div*/}
+        <GlassContainer className="md:hidden flex flex-col m-3 p-4 items-center justify-between">
+          <div className=" flex items-center justify-center p-4">
+            <Image
+              src="/assets/pd-logo.png"
+              alt="pd-logo.png"
+              width={160}
+              height={160}
+            ></Image>
+          </div>
+          <div className=" flex items-center justify-center">
+            <Image
+              src="/assets/pd-banner.png"
+              alt="pd-banner.png"
+              width={400}
+              height={200}
+            ></Image>
+          </div>
+          <div>
+            <p className="text-xs font-bold">where great minds compile.</p>
+          </div>
+        </GlassContainer>
+      </div>
+    </>
+  );
 }
---
+++ components/button/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClick?: () => void;
+}
+const Button = ({ children, onClick }: Props) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+export default Button;
