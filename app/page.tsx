ba56cb98aa3e70922e7dd5ba5785a4860efb864f
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2025-08-07 21:27:56 +0800

    added view folder and moved half of the components there, adjustment of colors and padding.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 
+import Link from "next/link"
+
 export default function Page() {
   return (
     <div className="min-h-screen bg-black text-white container mx-auto px-4 py-4">
       <h1>test 123</h1>
-      <a href="/projects" className="inline-block mt-4 px-4 py-2 bg-white text-black rounded hover:bg-gray-200 transition">Go to Projects Page</a>
+      <Link href="/projects" className="inline-block mt-4 px-4 py-2 bg-white text-black rounded hover:bg-gray-200 transition">
+        Go to Projects Page
+      </Link>
     </div>
   )
 }
