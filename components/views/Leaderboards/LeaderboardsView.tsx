4542a710b7a220f24c08410514e2515e83b78b8f
Paula Joyce Ucol <peaksenut@gmail.com>
2025-08-13 10:28:44 +0800

    feat/style: update ui

--- components/views/Leaderboards/LeaderboardsView.tsx
+++ components/views/Leaderboards/LeaderboardsView.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Navbar from "@/components/global/navbar";
+import BackgroundSvg from "@/components/leaderboards-ui/backgroundSvg";
+import { GlassContainer } from "@/components/shared/glass-container";
 import { X } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
@@ -8,152 +10,87 @@ import { useState } from "react";
 export default function LeaderboardsView() {
   const [date, setDate] = useState<Date>(new Date());
 
-  const sectionPadding: string = "px-6 lg:px-16 pb-6 md:pb-10 py-4 md:py-6";
-  const sectionContentWidth: string = "max-w-7xl mx-auto";
-  const red: string = "#541c1c";
-  const lightgrey: string = "#787878";
-  const darkgrey: string = "#39393B";
-  const glassEffect: string = "backdrop-blur-md bg-white/10 shadow-md";
+  const section: string = "pb-9 pt-8 rounded-none";
+  const sectionContentWidth: string = "max-w-7xl mx-auto px-8";
+  const glassEffect: string = "backdrop-blur-xl bg-black/10";
 
   return (
-    <main
-      className="min-h-screen text-white"
-      style={{
-        background: `linear-gradient(to bottom, ${red} 0%, var(--color-pd-black) 105%)`,
-      }}
-    >
-      {/* HEADER */}
-      <header className="pb-8.5 md:pb-9 pt-1 px-6 md:px-8 lg:px-16">
-        <div className={`${sectionContentWidth}`}>
-          {/* NAV CONTAINER */}
-          <Navbar />
-        </div>
-      </header>
+    <main className="relative min-h-screen text-white overflow-hidden">
+      <div className="absolute inset-0 -z-40  bg-pd-black flex items-center justify-center overflow-x-clip">
+        <BackgroundSvg className="h-full w-auto overflow-x-clip" />
+      </div>
 
-      {/* SUB-HEADER */}
-      <section
-        className={`bg-gradient-to-r from-[#787878] to-[#39393B] ${sectionPadding}`}
-      >
-        <div className={`md:space-y-1 ${sectionContentWidth}`}>
-          <span className="flex items-center">
-            <Image
-              src="/assets/pd-logo.png"
-              alt="PD Logo"
-              width={52}
-              height={35}
-              className="rounded-lg"
-            />
-            <X className="text-black" />
-            <Image
-              src="/assets/csc-logo.png"
-              alt="PD Logo"
-              width={52}
-              height={35}
-              className="rounded-lg"
-            />
-          </span>
-          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
-            Leaderboards
-          </h1>
-          <p className="md:text-xl lg:text-2xl">
-            Check who’s winning the race!
-          </p>
-        </div>
-      </section>
-
-      {/* LEADERBOARD */}
-      <section className={`${sectionPadding}`}>
-        <div className={`md:mt-12 lg:mt-18 ${sectionContentWidth}`}>
-          <p className="flex justify-end text-sm md:text-xl mt-4 md:mt-0">
-            Last updated: {date.toLocaleString()}
-          </p>
-          <div
-            className={`bg-[#787878] rounded-lg py-4 px-6 h-[20rem] lg:h-[35rem]`}
-          >
-            -asdas
+      <div className="z-40">
+        {/* HEADER */}
+        <header className="my-12 md:my-16">
+          <div className={`${sectionContentWidth}`}>
+            {/* NAV CONTAINER */}
+            <Navbar />
           </div>
-        </div>
-      </section>
+        </header>
 
-      {/* CCS Page Promotion */}
-      <div>
-        <svg
-          viewBox="0 0 1439 181"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-full h-auto"
-          preserveAspectRatio="xMidYMid slice"
-        >
-          <defs>
-            <linearGradient
-              id="horizontalGradient"
-              x1="0"
-              y1="0"
-              x2="1439"
-              y2="0"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop
-                offset="0"
-                stopColor={`#787878`}
-              />
-              <stop
-                offset="1"
-                stopColor={`#39393B`}
-              />
-            </linearGradient>
-          </defs>
-          <path
-            d="M1439 0V181H-1C628.435 177.093 1001.3 170.643 1439 0Z"
-            fill="url(#horizontalGradient)"
-          />
-        </svg>
+        <section className="space-y-16 lg:space-y-32 mb-16 md:mb-32">
+          {/* SUB-HEADER */}
+          <GlassContainer className={`${section} ${glassEffect}`}>
+            <div className={`space-y-2 ${sectionContentWidth}`}>
+              <span className="flex items-center">
+                <Image
+                  src="/assets/pd-logo.png"
+                  alt="PD Logo"
+                  width={52}
+                  height={35}
+                  className="rounded-lg"
+                />
+                <X className="text-red-700" />
+                <Image
+                  src="/assets/csc-logo.png"
+                  alt="CSC Logo"
+                  width={52}
+                  height={35}
+                  className="rounded-lg"
+                />
+              </span>
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold bg-gradient-to-r from-white to-[#791212] bg-clip-text text-transparent">
+                QuiCCStions Overdrive
+              </h1>
+              <p className="md:text-xl lg:text-2xl">
+                Check who’s winning the race!
+              </p>
+            </div>
+          </GlassContainer>
 
-        <section
-          className={`${sectionPadding}`}
-          style={{
-            background: `linear-gradient(to right, #787878, #39393B)`,
-          }}
-        >
-          <div className={`md:px-10 ${sectionContentWidth}`}>
-            <p className="md:text-xl font-bold">
-              For updates, follow the official CCS Student Council Page!
-            </p>
-            <div className="bg-white py-4 px-6 h-[20rem] lg:h-[35rem]">-</div>
-          </div>
+          {/* LEADERBOARD */}
+          <GlassContainer className={`${section} ${glassEffect}`}>
+            <div className={`my-5 lg:my-8 ${sectionContentWidth}`}>
+              <p className="flex justify-end text-sm md:text-xl mt-4 md:mt-0 mb-2 md:">
+                Last updated: {date.toLocaleString()}
+              </p>
+              <GlassContainer
+                className={`rounded-4xl h-[20rem] lg:h-[38rem] bg-black/50`}
+              >
+                <GlassContainer className="rounded-[1.3rem] mx-2 md:mx-5 my-3 md:my-6 border-0 bg-black/10">
+                  <h2 className="text-center my-2 md:my-4 text-3xl md:text-5xl font-bold bg-gradient-to-r from-white to-[#791212] bg-clip-text text-transparent">
+                    Leaderboards
+                  </h2>
+                </GlassContainer>
+              </GlassContainer>
+            </div>
+          </GlassContainer>
+
+          {/* CCS PAGE PROMOTION */}
+          <GlassContainer className={`${section} ${glassEffect} relative`}>
+            <div className={`my-5 lg:my-8 relative ${sectionContentWidth}`}>
+              <p className="text-sm md:text-xl mt-4 md:mt-0 mb-2">
+                For updates, follow the official CCS Student Council Page!
+              </p>
+              <GlassContainer
+                className={`rounded-4xl h-[20rem] lg:h-[35rem] bg-black/50`}
+              >
+                <div>FB EMBED</div>
+              </GlassContainer>
+            </div>
+          </GlassContainer>
         </section>
-
-        <svg
-          viewBox="0 0 1440 199"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-full h-auto"
-          preserveAspectRatio="xMidYMid slice"
-        >
-          <defs>
-            <linearGradient
-              id="horizontalGradient2"
-              x1="0"
-              y1="0"
-              x2="1440"
-              y2="0"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop
-                offset="0"
-                stopColor={`#787878`}
-              />
-              <stop
-                offset="1"
-                stopColor={`#39393B`}
-              />
-            </linearGradient>
-          </defs>
-          <path
-            d="M274.031 33.7476C110.395 21.4591 5.1637 125.66 -2 199V0H1440C1391.56 28.3219 1251.23 130.513 1057.38 73.5002C863.787 16.5641 674.248 59.7884 584.755 104.014C493.51 149.106 446.635 46.7095 274.031 33.7476Z"
-            fill="url(#horizontalGradient2)"
-          />
-        </svg>
       </div>
     </main>
   );
