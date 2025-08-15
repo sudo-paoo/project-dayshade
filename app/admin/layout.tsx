0c4b527e76b0c8841b95c8dcd4e64d2a2bdfd921
DreiDev04 <tacujan.andrei@gmail.com>
2025-08-15 15:22:19 +0800

    Added: Admin Panel

---
+++ app/admin/layout.tsx
@@ -0,0 +1,32 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          {/* //! DAPAT HINDI TEXT-WHITE */}
+          <h1 className="text-lg font-semibold text-white">Progden Admin Panel</h1>
+        </header>
+
+        <div className="flex flex-1 flex-col gap-4 p-4 ">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
