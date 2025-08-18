9bae715dafa7db801b1462079f311b88cf4ba1bf
Meow <108517390+MeowMeow0417@users.noreply.github.com>
2025-08-18 13:17:53 +0800

    added: Admin Dashboard header title and description + Initial push

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,34 @@
+'use client'
+
 import React from 'react'
+import {Label} from "@/components/ui/label";
+import { Switch } from '@/components/ui/switch';
+import {Card, CardContent} from "@/components/ui/card";
+
 
 const page = () => {
   return (
-    <div className='text-white '>
-      
-    </div>
+    <section>
+      <header className="flex items-center justify-between p-4">
+        {/* The Label stuff */}
+        <div className="flex flex-col gap-2">
+            <Label className='text-2xl font-bold' htmlFor="dashboard">
+              Dashboard
+            </Label>
+            <p>Manage the Page</p>
+        </div>
+
+        {/* Recruitment button thing */}
+        <div className='flex items-center gap-4'>
+          <Label className='text-lg'>Recruitment</Label>
+          <div className='flex items-center gap-2 border-1 rounded-md p-4 bg-card'>
+            <Switch id="recruitment" />
+            <Label htmlFor="recruitment">Active/Not Active</Label>
+          </div>
+        </div>
+      </header>
+
+    </section>
   )
 }
 
