6e5d6b47830689f69b2a537dacf210ff4c1a62dd
sudo-paoo <kingpaolofranco60@gmail.com>
2025-09-03 08:39:30 +0800

    fix join button not working on mobile

--- components/global/header.tsx
+++ components/global/header.tsx
@@ -64,7 +64,7 @@ export function NavbarDemo() {
             ))}
             <div className="flex w-full flex-col gap-4">
               <NavbarButton
-                onClick={() => setIsMobileMenuOpen(false)}
+                 href="/join"
                 variant="primary"
                 className="w-full"
               >
